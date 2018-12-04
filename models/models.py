148a9dfda3ee46f7e2cd80961667b858489de8d0
David Moreno <david.moreno0440@gmail.com>
2018-12-04 20:31:44 +0100

    Cálculo completo de equipos y plantones en la hoja de proyecto

--- models/models.py
+++ models/models.py
@@ -42,10 +42,15 @@ class ProyectoLine(models.Model):
 	_descripcion = "Linea plantas en proyecto de Reforestación"
 
 	name = fields.Many2one('planta', "Tipo de Planta")
+	coste = fields.Monetary("Precio", help="Precio de cada plantón", readonly=True, store=True, compute="_get_coste")
 	cantidad = fields.Integer("Cantidad")
 	precio_subtotal = fields.Monetary("Subtotal", compute="_get_subtotal", store=True)
 	proyecto_id = fields.Many2one('proyecto', "Pertenece a ", ondelete="cascade")
 	currency_id = fields.Many2one('res.currency', "Moneda", default=lambda self: self.env['res.currency'].browse(1))
+
+	@api.depends('name')
+	def _get_coste(self):
+		self.coste = self.name.coste_produccion
 
 	@api.depends('cantidad', 'name')
 	def _get_subtotal(self):
@@ -91,8 +96,7 @@ class ProyectoEquipo(models.Model):
 	coste = fields.Monetary("Coste Jornada", help="Coste del Equipo por una jornada completa")
 	color = fields.Selection((('rojo',"Rojo"), ('verde',"Verde"),('amarillo',"Amarillo"),('azul',"Azul")), "Color")
 	notas = fields.Text("Notas")
-	proyecto_id = fields.One2many('proyecto', 'equipo_trabajadores', string="Pertenece a ", readonly=True)
-	currency_id = fields.Many2one('res.currency', "Moneda", default=lambda self: self.env['res.currency'].search([('name','=','EUR')]))
+	currency_id = fields.Many2one('res.currency', "Moneda", default=lambda self: self.env['res.currency'].search([('name','=','EUR')]), readonly=True)
 
 	
 
@@ -111,9 +115,31 @@ class ProyectoEquipo(models.Model):
 			raise ValidationError("El responsable no puede se un componente del equipo. Por favor, elige otro responsable o elimínalo como componente del equipo de trabajo")
 
 
+class ProyectoEquipoLinea(models.Model):
+	_name = 'proyecto.equipo.linea'
+	_description = "Cada una de las linea de trabajo de un proyecto asociada a un equipo"
+
+	name = fields.Many2one('proyecto.equipo', required=True)
+	coste_dias = fields.Monetary("Coste Jornada", compute="_get_coste", readonly=True, store=True)
+	currency_id = fields.Many2one('res.currency', string='Moneda', default=lambda self: self.env['res.currency'].search([('name','=','EUR')]), readonly=True)
+	numero_dias = fields.Integer("Número Jornadas")
+	subtotal = fields.Monetary("Subtotal", compute="_get_subtotal", store=True)
+	proyecto_id = fields.Many2one('proyecto',"Pertenede a ", readonly=True)
+
+	@api.depends('name')
+	def _get_coste(self):
+		self.coste_dias = self.name.coste
+
+	@api.depends('coste_dias','numero_dias')
+	def _get_subtotal(self):
+		self.subtotal = self.coste_dias * self.numero_dias
+
+
 class Proyecto(models.Model):
 	_name = 'proyecto'
 	_description = "Proyecto de Reforestación"
+
+	# Antes de guardas el proyecto parece que muestras todo, incluyendo plantas y equipos
 
 	name = fields.Char("Nombre Proyecto", required=True)
 	poblacion = fields.Char("Poblacion")
@@ -122,13 +148,15 @@ class Proyecto(models.Model):
 	lng = fields.Float("Longitud", digits=(10,8))
 	inversion = fields.Monetary("Inversión", help="Cantidad en Euros destinada al proyecto")
 	origen_inversion = fields.Char("Origen Inversión", help="Entidad pública o privada que aporta el dinero necesario para el proyecto.")
-	currency_id = fields.Many2one('res.currency', string='Moneda', default=lambda self: self.env['res.currency'].search([('name','=','EUR')]))
+	currency_id = fields.Many2one('res.currency', string='Moneda', default=lambda self: self.env['res.currency'].search([('name','=','EUR')]), readonly=True)
 	proyecto_lines_ids = fields.One2many('proyecto.line', 'proyecto_id', string="Plantas Escogidas")
 	inicio_proyecto = fields.Date("Fecha Inicio")
 	final_proyecto = fields.Date("Fecha final estimada")
 	responsable = fields.Many2one('res.users', "Responsable", required=True)
-	equipo_trabajadores = fields.Many2one('proyecto.equipo', string="Equipo de Trabajo")
+	equipos_trabajadores_lineas = fields.One2many('proyecto.equipo.linea', 'proyecto_id', string="Equipo de Trabajo")
 	estado_id = fields.Many2one('proyecto.estado', "Estado Proyecto")
+	subtotal_equipos = fields.Monetary("Subtotal Equipos", compute="_get_subtotal_equipos", store=True )
+	subtotal_plantas = fields.Monetary("Subtotal Plantones", compute="_get_subtotal_plantas", store=True)
 	precio_total = fields.Monetary("Precio Total", compute="_get_total", store=True)
 	estudio_terreno = fields.Binary("Estudio del Terreno")
 	estudio_terreno_filename = fields.Char("Estudio del Terreno filename")
@@ -163,20 +191,35 @@ class Proyecto(models.Model):
 		self.estado_id = estado_id.id
 
 
-	@api.depends('proyecto_lines_ids', 'precio_total')
-	def _get_total(self):	
+	@api.depends('proyecto_lines_ids')
+	def _get_subtotal_plantas(self):	
 		for record in self:
 			total = 0
 			for line in record.proyecto_lines_ids:
 				total += line.precio_subtotal
-			record.precio_total = total
+			record.subtotal_plantas = total
+
+	@api.depends('equipos_trabajadores_lineas')
+	def _get_subtotal_equipos(self):	
+		for record in self:
+			total = 0
+			for line in record.equipos_trabajadores_lineas:
+				total += line.subtotal
+			record.subtotal_equipos = total
+
+	@api.depends('subtotal_equipos', 'subtotal_plantas')
+	def _get_total(self):
+		for record in self:
+			total = 0
+			record.precio_total = record.subtotal_equipos + record.subtotal_plantas
+
+			
 
 	@api.onchange('responsable', 'equipo_trabajadores')
 	def _onchange_check_responsable(self):
 
-		for trabajador in self.equipo_trabajadores.componentes:
-			_logger.info(trabajador)
-
+		for trabajador in self.equipos_trabajadores_lineas.name.componentes:
+	
 			if trabajador.email == self.responsable.login:
 				return {'warning':{
 					'title': "El responsable (" + self.responsable.name + ") no puede ser uno de los operarios",
@@ -185,7 +228,7 @@ class Proyecto(models.Model):
 
 	@api.constrains('responsable', 'equipo_trabajadores')
 	def _check_responsable(self):
-		for trabajador in self.equipo_trabajadores.componentes:
+		for trabajador in self.equipos_trabajadores_lineas.name.componentes:
 			if trabajador.email == self.responsable.login:
 				raise ValidationError(_("El responsable (" + self.responsable.name + ") no puede ser uno de los operarios"))
 
